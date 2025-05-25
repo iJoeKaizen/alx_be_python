fcc7e6321199d8997b772cde3ec5ebd60d27b2dc
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-25 21:41:26 +0000

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,8 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05  # Interest rate in percent
+time = 3  # Time in years
+
+# Calculate simple interest
+interest = principal * rate * time
+print(f"The simple interest is: {interest:.1f}")
 
